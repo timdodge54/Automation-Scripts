907deaf1d03e045c688fdbf611118c2fde5f6a73
Tim Dodge <tim.dodge64@gmail.com>
2023-01-25 18:01:23 -0700

    renamed script

--- gpt_master_script.py
+++ gpt_master_script.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import subprocess
 
@@ -8,9 +9,15 @@ run_scores = {}
 exceeded_limit = {}
 
 # Get file paths.
-student_path = input("Please enter the path (likely starting with C:/) of the student files: ")
-input_path = input("Please enter the path of the input files (in the form Input***.txt): ")
-compiler_type = input("Please indicate the compiler you would like to use (1 = CygWin and gcc, 2 = VisualStudio and cl)")
+student_path = input(
+    "Please enter the path (likely starting with C:/) of the student files: "
+)
+input_path = input(
+    "Please enter the path of the input files (in the form Input***.txt): "
+)
+compiler_type = input(
+    "Please indicate the compiler you would like to use (1 = CygWin and gcc, 2 = VisualStudio and cl)"
+)
 
 # Get the list of input files.
 os.chdir(input_path)
@@ -50,7 +57,9 @@ for file in os.listdir():
         # Print the filename for student's name reference.
         print(" ")
         print(file)
-        wc_results = subprocess.run(["wc", "-L", file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        wc_results = subprocess.run(
+            ["wc", "-L", file], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         line_length = int(wc_results.stdout.split()[0])
         if line_length > 80:
             exceeded_limit[file[:-2]] = f"Longest line length: {line_length}"
@@ -60,7 +69,12 @@ for file in os.listdir():
         # Run the student's program with each of the input files.
         for input_file in input_files:
             with open(f"{input_path}/{input_file}", "r") as input_data:
-                subprocess.run(["timeout", "2", "./"+name], stdin=input_data, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                subprocess.run(
+                    ["timeout", "2", "./" + name],
+                    stdin=input_data,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
                 print(" ")
                 with open("output.txt", "r") as output_data:
                     print(output_data.read())
@@ -68,4 +82,3 @@ for file in os.listdir():
 
         # Get and store the scores for this student's program.
         print("Scores and comments:")
-
--- test.py
+++ test.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 
-import subprocess
 import argparse
 import os
+import subprocess
 import sys
 from pathlib import Path
 
 print("Inside Python script")
 
 parser = argparse.ArgumentParser(
-    prog='Compiler',
-    description='''Compile and run a c++ program''',
-    epilog="None ya buisness..."
+    prog="Compiler",
+    description="""Compile and run a c++ program""",
+    epilog="None ya buisness...",
 )
 
-parser.add_argument('folder', help='Relative path to folder of cpp files to compile and run')
-parser.add_argument('input_path', help="Path of the input files (in the form Input***.txt): ")
-parser.add_argument('compiler_type', help="Indicate the compiler you would like to use (1 = CygWin and gcc, 2 = VisualStudio and cl)")
+parser.add_argument(
+    "folder", help="Relative path to folder of cpp files to compile and run"
+)
+parser.add_argument(
+    "input_path", help="Path of the input files (in the form Input***.txt): "
+)
+parser.add_argument(
+    "compiler_type",
+    help="Indicate the compiler you would like to use (1 = CygWin and gcc, 2 = VisualStudio and cl)",
+)
 
 args = parser.parse_args()
 folder = args.folder
 input_path = args.input_path
-compiler_type = args.input_path
+compiler_type = args.compiler_type
 
 
 source = Path(folder)
@@ -33,6 +40,5 @@ for i, file in enumerate(files):
     exec_loc = "a.out"
     print(f"exec_loc = {exec_loc}")
     subprocess.call(["g++", folder_file])
-    output = subprocess.run(f"./{exec_loc}", capture_output=True, shell=True)
-    print(f"stdout_{i} : {output.stdout.decode()}")
-    print(f"stderr: {output.stderr.decode()}")
+    output = subprocess.run([f"{exec_loc}", "1"], capture_output=True, shell=True)
+    print(output.stdout.decode())
